8d2baa1fa9bf2c46941ef84a549581c4cd6f8c38
mason <anomvaultx12@outlook.com>
2026-05-13 15:51:03 -0400

    Initial working JUnit smoke test setup

---
+++ src/test/java/com/veloce/ai/tests/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.veloce.ai.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmokeTest {
+
+    @Test
+    public void veloceAiSmokeTest() {
+        assertEquals(1, 1);
+    }
+}
