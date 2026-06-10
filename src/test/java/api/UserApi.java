606076ba4c22ccd3be713fb6b29f2eddffe7dec2
Fahim Ahmed <apex.fahimahmed@gmail.com>
2026-06-10 12:16:01 -0400

    refactor(api): align UserApi with RequestSpecification pattern

---
+++ src/test/java/api/UserApi.java
@@ -0,0 +1,34 @@
+package api;
+
+import config.RequestSpecFactory;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class UserApi {
+    private final RequestSpecification spec;
+
+    public UserApi(String baseUrl){
+        this.spec = RequestSpecFactory.getDefaultSpec(baseUrl);
+    }
+
+    public Response registerUser(String name, String email){
+        Map<String, String> requestBody = new HashMap<>();
+
+        requestBody.put("name", name);
+        requestBody.put("email", email);
+
+        return given()
+                .spec(spec)
+                .body(requestBody)
+                .when()
+                .post("/users/register");
+    }
+
+}
+
