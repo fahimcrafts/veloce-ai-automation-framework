fe9eee979923a730b7674c4de66e33dbec293aa4
Fahim Ahmed <apex.fahimahmed@gmail.com>
2026-06-16 21:36:22 -0400

    Add JDBC Select query validation for users table

---
+++ src/test/java/debug/UserQueryTest.java
@@ -0,0 +1,29 @@
+package debug;
+
+import utils.DBConnectionManager;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class UserQueryTest {
+    public static void main(String[] args) {
+        String query = "SELECT * FROM users";
+
+
+    try(
+        Connection connection = DBConnectionManager.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(query);
+    ){
+        while (resultSet.next()){
+            System.out.println("ID: " + resultSet.getInt("id")
+                    + ", Name: " + resultSet.getString("name")
+                    + ", Email: " + resultSet.getString("email"));
+        }
+    }
+    catch (Exception e){
+        e.printStackTrace();
+    }
+    }
+}
