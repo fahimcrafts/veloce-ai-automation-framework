054c33b9c620cf9f2e9988b9cf7d57889935f5c9
Fahim Ahmed <apex.fahimahmed@gmail.com>
2026-06-17 17:03:17 -0400

    Add field-level SQL validation for user name and email data

---
+++ src/test/java/tests/sql/UserSqlValidationTest.java
@@ -0,0 +1,22 @@
+package tests.sql;
+
+import database.UserDataRetriever;
+import models.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserSqlValidationTest {
+    @Test
+    public void shouldFetchUserByEmailFromDatabase(){
+        String email = "jeffn@email.com";
+
+        User dbUser = UserDataRetriever.getUserByEmail(email);
+
+        assertNotNull(dbUser);
+
+        assertEquals("Jeff Nippard", dbUser.getName());
+        assertEquals(email, dbUser.getEmail());
+    }
+}
